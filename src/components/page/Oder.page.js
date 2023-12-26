5bb1c34fb4554bd1a73a752fa96dddbba7adb78f
datbachan1 <hjhjhj2367@gmail.com>
2023-12-26 11:50:32 +0700

    fix oder_page

--- src/components/page/Oder.page.js
+++ src/components/page/Oder.page.js
@@ -1,19 +1,24 @@
+import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import yup from "../YupGlobal";
 import { useDispatch, useSelector } from "react-redux";
 import { http } from "../../api/http";
 import { clearCart } from "../../features/cart/CartSlice";
-const schema = yup
-  .object({
-    name: yup.string().min(1).required(),
-    note: yup.string().min(1).required(),
-    address: yup.string().required(),
-    phone: yup.number().min(10).required(),
-  })
-  .required();
+
+const schema = yup.object({
+  name: yup.string().min(1).required(),
+  note: yup.string().min(1).required(),
+  address: yup.string().required(),
+  phone: yup.number().min(10).required(),
+}).required();
 
 const Order = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.carts);
+  const total = useSelector((state) => state.cart.total);
+  const userData = useSelector((state) => state.auth.userData);
+
   const {
     register,
     handleSubmit,
@@ -22,10 +27,7 @@ const Order = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const dispatch = useDispatch();
-  const cartItems = useSelector((state) => state.cart.carts);
-  const total = useSelector((state) => state.cart.total);
-  const userData = useSelector((state) => state.auth.userData);
+
   const onSubmit = async (data) => {
     const orderData = {
       userId: userData._id,
@@ -36,12 +38,14 @@ const Order = () => {
       cartItems: cartItems,
       total: total,
     };
+
     try {
       const response = await http.request({
         method: "POST",
-        url: "/oder",
+        url: "/order",
         data: orderData,
       });
+
       if (response.message === "success") {
         dispatch(clearCart());
         reset();
@@ -55,10 +59,10 @@ const Order = () => {
     <section className="order container" style={{ marginBlock: 100 }}>
       <div className="order-wrapper">
         <div className="order-left">
-          <h3 className="oder-title">SECURE CHECKOUT</h3>
-          <form onSubmit={handleSubmit(onSubmit)} className="oder-form">
-            <div className="oder-form-box">
-              <h3 className="oder-form-title">Shipping Address</h3>
+          <h3 className="order-title">SECURE CHECKOUT</h3>
+          <form onSubmit={handleSubmit(onSubmit)} className="order-form">
+            <div className="order-form-box">
+              <h3 className="order-form-title">Shipping Address</h3>
               <div className="form-item">
                 <label htmlFor="name">Name*</label>
                 <input id="name" {...register("name")} />
@@ -68,16 +72,16 @@ const Order = () => {
                 <label htmlFor="address">Address*</label>
                 <input id="address" {...register("address")} />
                 <p className="form-error">{errors.address?.message}</p>
-                <div className="form-item">
-                  <label htmlFor="phone">Phone Number*</label>
-                  <input id="phone" {...register("phone")} type="number" />
-                  <p className="form-error">{errors.phone?.message}</p>
-                </div>
-                <div className="form-item">
-                  <label htmlFor="note">Note for shipper*</label>
-                  <input id="note" {...register("note")} placeholder="" />
-                  <p className="form-error">{errors.note?.message}</p>
-                </div>
+              </div>
+              <div className="form-item">
+                <label htmlFor="phone">Phone Number*</label>
+                <input id="phone" {...register("phone")} type="number" />
+                <p className="form-error">{errors.phone?.message}</p>
+              </div>
+              <div className="form-item">
+                <label htmlFor="note">Note for shipper*</label>
+                <input id="note" {...register("note")} placeholder="" />
+                <p className="form-error">{errors.note?.message}</p>
               </div>
             </div>
             <button className="form-submit" type="submit">
@@ -86,47 +90,47 @@ const Order = () => {
           </form>
         </div>
         <div className="order-right">
-          <h3 className="oder-title">ORDER SUMMARY</h3>
-          <div className="oder-items">
+          <h3 className="order-title">ORDER SUMMARY</h3>
+          <div className="order-items">
             <span>Items</span>
             <span>{cartItems.length}</span>
           </div>
-          <div className="oder-items">
+          <div className="order-items">
             <span>Estimated Tax</span>
             <span>Calculated in Checkout</span>
           </div>
-          <div className="oder-items">
+          <div className="order-items">
             <p>Shipping</p>
             <p>Free</p>
           </div>
-          <div className="oder-total">
+          <div className="order-total">
             <span>Total</span>
             <span>${total}</span>
           </div>
-          {userData ? (
-            <div className="oder-cart">
-              <div className="oder-cart-head">
+          {userData && (
+            <div className="order-cart">
+              <div className="order-cart-head">
                 <p>Shopping Bag</p>
               </div>
 
-              <div className="oder-cart-number">
+              <div className="order-cart-number">
                 {cartItems.length === 1
                   ? `${cartItems.length} item`
                   : `${cartItems.length} items`}
               </div>
-              <div className="oder-cart-lists">
+              <div className="order-cart-lists">
                 {cartItems?.map((item) => (
-                  <div className="oder-cart-items" key={item._id}>
-                    <div className="oder-cart-img">
-                      <img src={item.images[0]} alt="cart oder" />
+                  <div className="order-cart-items" key={item._id}>
+                    <div className="order-cart-img">
+                      <img src={item.images[0]} alt="cart order" />
                     </div>
-                    <div className="oder-cart-desc">
-                      <p className="oder-cart-name">{item.name}</p>
-                      <p className="oder-cart-price">{`$${item.price}`}</p>
-                      <p className="oder-cart-size">
+                    <div className="order-cart-desc">
+                      <p className="order-cart-name">{item.name}</p>
+                      <p className="order-cart-price">{`$${item.price}`}</p>
+                      <p className="order-cart-size">
                         {item.size} <span>Quantity: {item.quantity}</span>
                       </p>
-                      <p className="oder-cart-subTotal">
+                      <p className="order-cart-subTotal">
                         Total <span>${item.subTotal}</span>
                       </p>
                     </div>
@@ -134,8 +138,6 @@ const Order = () => {
                 ))}
               </div>
             </div>
-          ) : (
-            ""
           )}
         </div>
       </div>
