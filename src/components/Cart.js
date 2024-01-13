d6b94c1a14bd0ac7a1e2f9e55fc3c0d51c1c4f27
datbachan1 <hjhjhj2367@gmail.com>
2024-01-13 14:40:45 +0700

    fix header

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,75 +1,21 @@
 import React from "react";
 import { GrClose } from "react-icons/gr";
+
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { removeFromCart } from "../features/cart/CartSlice";
 import { isValidAccessToken } from "../utils/cookieStorage";
 import { ROUTES } from "./../utils/routes";
 import { openLoginModal } from "../features/modal/modalSlice";
-
-const CartItem = ({ item, onRemoveItem }) => (
-  <div className="cart-product" key={`product-inBagCart-${item.id}`}>
-    <div className="cart-img">
-      <img src={item.images[0]} alt="" />
-    </div>
-    <div className="cart-desc">
-      <p className="cart-name">
-        {item.name}{" "}
-        <GrClose
-          style={{ cursor: "pointer" }}
-          onClick={() => onRemoveItem(item.id)}
-        />
-      </p>
-      <p className="cart-decription">Odeon - Medium Wash</p>
-      <p className="cart-price">
-        {`$${item.price}`} <del>89.50</del>
-      </p>
-      <div className="cart-size">
-        <p>Size: {item.size}</p>
-        <span className="w-full p-2 border border-gray-300 rounded">
-          Quantity: {item.quantity}
-        </span>
-      </div>
-      <div className="cart-subtotal">
-        <p>
-          Subtotal: <span>${item.subTotal}</span>
-        </p>
-      </div>
-    </div>
-  </div>
-);
-
 const Cart = ({ open, onClose }) => {
   const navigate = useNavigate();
+
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.carts);
   const total = useSelector((state) => state.cart.total);
-
   const handleRemoveItem = (itemId) => {
     dispatch(removeFromCart(itemId));
   };
-
-  const handleContinueCheckout = (e) => {
-    e.preventDefault();
-    const isValidToken = isValidAccessToken();
-
-    if (isValidToken) {
-      navigate(ROUTES.ORDER);
-    } else {
-      dispatch(openLoginModal());
-    }
-  };
-
-  const renderCartItems = () =>
-    cartItems?.map((item) => (
-      <CartItem key={item.id} item={item} onRemoveItem={handleRemoveItem} />
-    ));
-
-  const renderTotal = () => (
-    <div className="cart-total-title">
-      Estimated Total <span>{`$${total}`}</span>
-    </div>
-  );
 
   return (
     <div className="cart-root">
@@ -80,12 +26,55 @@ const Cart = ({ open, onClose }) => {
         </p>
         <div className={cartItems?.length === 3 ? "cart-group" : ""}>
           <h4 className="cart-ship">Ship</h4>
-          {renderCartItems()}
+          {/* product item bag cart here */}
+          {cartItems?.map((item) => (
+            <div className="cart-product" key={`product-inBagCart-${item.id}`}>
+              <div className="cart-img">
+                <img src={item.images[0]} alt="" />
+              </div>
+              <div className="cart-desc">
+                <p className="cart-name">
+                  {item.name}{" "}
+                  <GrClose
+                    style={{ cursor: "pointer" }}
+                    onClick={() => handleRemoveItem(item.id)}
+                  />
+                </p>
+                <p className="cart-decription">Odeon - Medium Wash</p>
+                <p className="cart-price">
+                  {`$${item.price}`} <del>89.50</del>
+                </p>
+                <div className="cart-size">
+                  <p>Size: {item.size}</p>
+                  <span className="w-full p-2 border border-gray-300 rounded">
+                    Quantity: {item.quantity}
+                  </span>
+                </div>
+                <div className="cart-subtotal">
+                  <p>
+                    Subtotal: <span>${item.subTotal}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
         <div className="cart-total">
-          {renderTotal()}
+          <div className="cart-total-title">
+            Estimated Total <span>{`$${total}`}</span>
+          </div>
           <div className="cart-btn">
-            <Link to="/" onClick={handleContinueCheckout}>
+            <Link
+              to="/"
+              onClick={(e) => {
+                e.preventDefault();
+                const isValidToken = isValidAccessToken();
+
+                if (isValidToken) return navigate(ROUTES.ORDER);
+
+                dispatch(openLoginModal());
+              }}
+            >
               Continue to checkout
             </Link>
           </div>
