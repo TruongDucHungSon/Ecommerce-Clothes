d27b1db150cba0e04d6ed6810b2ac087a335b429
truonglam205 <154526292+truonglam205@users.noreply.github.com>
2024-01-14 13:11:50 +0700

    Update productSlice.js

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,6 +1,7 @@
 // src/redux/apiSlice.js
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { product } from "../../services/product.service";
+import axios from "axios";
 
 export const fetchApiData = createAsyncThunk(
   "api/fetchApiData", // Use a unique action type here
@@ -38,10 +39,37 @@ export const fetchProductDetail = createAsyncThunk(
   }
 );
 
+export const filterProduct = createAsyncThunk(
+  "products/filterProduct",
+  async ({name, minPrice, maxPrice, brand}) => {
+    try {
+      let query = "https://ecommerce-api-mcqr.onrender.com/product/filterProduct?";
+      if(!!name) {
+        query += "name=" + name;
+      }
+      if(!!minPrice) {
+        query += "&minPrice=" + minPrice;
+      }
+      if(!!maxPrice) {
+        query += "&maxPrice=" + maxPrice;
+      }
+      if(!!brand) {
+        query += "&brand=" + brand;
+      }
+      const response = await axios.get(query);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+);
+
 const apiSlice = createSlice({
   name: "api",
   initialState: {
     data: [],
+    searchResults: [],
+    searchResultsLoading: false,
     loading: false,
     error: null,
     productDetail: null,
@@ -54,6 +82,9 @@ const apiSlice = createSlice({
   reducers: {
     setActiveImageIndex: (state, action) => {
       state.activeImageIndex = action.payload;
+    },
+    updateSearchResults: (state, action) => {
+      state.searchResults = action.payload;
     },
   },
   extraReducers: (builder) => {
@@ -94,11 +125,23 @@ const apiSlice = createSlice({
       .addCase(fetchProductByCategory.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
+      })
+      .addCase(filterProduct.pending, (state) => {
+        state.error = null;
+        state.searchResultsLoading = true;
+      })
+      .addCase(filterProduct.fulfilled, (state, action) => {
+        state.searchResults = action.payload;
+        state.searchResultsLoading = false;
+      })
+      .addCase(filterProduct.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.searchResultsLoading = false;
       });
   },
 });
 
-export const { setActiveImageIndex } = apiSlice.actions;
+export const { setActiveImageIndex, updateSearchResults } = apiSlice.actions;
 
 export const selectproducts = (state) => state.api.data.products;
 
